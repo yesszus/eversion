36ef61382068648f327ee041a90b927eec5fdc24
Gábor <yesszus@gmail.com>
2016-10-07 12:16:15 +0200

    Add files via upload

---
+++ languages/Hungarian_global.php
@@ -0,0 +1,53 @@
+<?php
+define("EVERSION_SUMMARY", 		"Plugin Verzió Ellenőrző");    // needed in plugin.xml 
+define("EVERSION_DESC", 			"Plugin Verzió Ellenőrző");    // needed in plugin.xml
+define("EVERSION_LINKTITLE", 	"Plugin Frissítések");    // needed in plugin.xml
+
+define("EVERSION_2",					"Legújabb Verzió");   // needed in plugin menu
+define("EVERSION_3",					"Nincs frissítés");        // needed in plugin menu
+
+
+define("EVERSION_1","Plugin Verzió");
+define("EVERSION_2","Legújabb Verzió");
+define("EVERSION_3","Nincs frissítés");
+define("EVERSION_4","Plugin Legújabb Verzió");
+
+define("EVERSION_5","Plugin Neve");
+define("EVERSION_6","Plugin");
+define("EVERSION_7","Verzió");
+define("EVERSION_8","RC");
+define("EVERSION_9","Béta Verzió");
+define("EVERSION_10","Verzió dátuma");
+define("EVERSION_11","Plugin Szerzője");
+define("EVERSION_12","Revisions");
+define("EVERSION_13","Developer Comments");
+define("EVERSION_14","Letöltés");
+define("EVERSION_15","Go to Download");
+define("EVERSION_16","Plugin Version Details");
+define("EVERSION_17","Plugin Lista");
+define("EVERSION_18","Go to Page : ");
+define("EVERSION_19","Support Forum");
+define("EVERSION_20","Go to Forum");
+
+define("EVERSION_21","Found in Plugin ");
+define("EVERSION_22","Title");
+define("EVERSION_23","This version created :");
+define("EVERSION_24","No download available");
+define("EVERSION_25","No support link defined");
+define("EVERSION_26","No items");
+define("EVERSION_27","No bugtracker defined");
+define("EVERSION_28","Bugtracker");
+define("EVERSION_29","Go to Bugtracker");
+define("EVERSION_30","Plugin Frissítések");
+define("EVERSION_31","Vissza");
+define("EVERSION_32","Fel");
+
+define("EVERSION_RSS01","everzió");
+define("EVERSION_RSS02","RSS feed for eversion");
+define("EVERSION_RSS03","Verzió");
+define("EVERSION_RSS04","is now available.");
+define("EVERSION_RSS05","You are not permitted to view this feed");
+define("EVERSION_RSS06","is now available.");
+define("EVERSION_RSS07","Latest Plugin Release for");
+define("EVERSION_RSS08","This is a beta release.");
+define("EVERSION_RSS09","Release date -");
